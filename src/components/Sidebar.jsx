1622b10bcf48077fe30fa22b1d12c5d303de5e8d
yosephkevinh02 <yosephkevinh02@gmail.com>
2024-09-18 18:02:51 +0700

    update sidebar and header

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { IoMdArrowForward } from 'react-icons/io';
+import { FiTrash2 } from 'react-icons/fi';
+import CartItem from './CartItem';
+import { SidebarContext } from '../contexts/SidebarContext';
 
 const Sidebar = () => {
-    return <div>Sidebar</div>;
+
+    const { isOpen, handleClose } = useContext(SidebarContext);
+
+    return (
+        <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed to-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+            <div className='flex justify-between items-center py-6 border-b'>
+                <div className='uppercase text-sm font-semibold'>
+                    Shopping Bag (0)
+                </div>
+                <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
+                    <IoMdArrowForward className='text-2xl' onClick={handleClose} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ProductProvider from './contexts/ProductContext';
+import SidebarProvider from './contexts/SidebarContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ProductProvider>
-    <React.StrictMode>
+  <SidebarProvider>
+    <ProductProvider>
       <App />
-  </React.StrictMode>
-  </ProductProvider>
+    </ProductProvider>
+  </SidebarProvider>
 );
