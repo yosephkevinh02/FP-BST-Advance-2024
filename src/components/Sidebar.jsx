4b219f53fde56d604d8a2bd44ab8dcd30c1f8ea4
yosephkevinh02 <yosephkevinh02@gmail.com>
2024-09-18 23:28:26 +0700

    update cart context

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,10 +4,13 @@ import { IoMdArrowForward } from 'react-icons/io';
 import { FiTrash2 } from 'react-icons/fi';
 import CartItem from './CartItem';
 import { SidebarContext } from '../contexts/SidebarContext';
+import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
 
     const { isOpen, handleClose } = useContext(SidebarContext);
+
+    const {cart} = useContext(CartContext);
 
     return (
         <div className={`${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed to-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
@@ -18,6 +21,11 @@ const Sidebar = () => {
                 <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
                     <IoMdArrowForward className='text-2xl' onClick={handleClose} />
                 </div>
+            </div>
+            <div>
+                {cart.map(item => {
+                    return <CartItem item={item} key={item.id} />
+                })}
             </div>
         </div>
     );
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { useState, useEffect, createContext } from 'react';
 
-const CartContext = () => {
-    return <div>CartContext</div>;
+export const CartContext = createContext();
+
+const CartProvider = ({children}) => {
+
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (product, id) => {
+        const newItem = {...product, amount: 1};
+        
+        const cartItem = cart.find(item => {
+            return item.id === id
+        });
+
+        if (cartItem) {
+            const newCart = [...cart].map(item => {
+                if (item.id === id) {
+                    return {...item, amount: cartItem.amount + 1}
+                } else {
+                    return item;
+                }
+            });
+            setCart(newCart);
+        } else {    
+            setCart([...cart, newItem]);
+        }
+    };
+
+    return (
+        <CartContext.Provider value={{cart, addToCart}}>
+            {children}
+        </CartContext.Provider>
+    );
 };
 
-export default CartContext;
+export default CartProvider;
