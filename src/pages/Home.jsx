d398c08659bd169778a314b7303d567a2c4dd26d
yosephkevinh02 <yosephkevinh02@gmail.com>
2024-09-19 12:49:30 +0700

    update product details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
     const { products } = useContext(ProductContext);
     const filteredProducts = products.filter((item) => {
-        return item.category === "men's clothing" || item.category === "women's clothing";
+        return item.category;
     });
 
     return (
