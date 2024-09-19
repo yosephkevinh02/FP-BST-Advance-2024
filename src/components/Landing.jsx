63b45a96deed6343d341bd41f08e73246004e697
yosephkevinh02 <yosephkevinh02@gmail.com>
2024-09-19 17:52:29 +0700

    edit landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,30 +1,31 @@
-import { Link } from 'react-router-dom';
 import Phone from '../img/app-phone.png';
 
 
-const Character = () => {
+const Landing = ({ productsRef }) => {
     return (
-        <section className=" h-[800px] bg-home bg-no-repeat bg-cover bg-center py-24">
-            <div className="container mx-auto flex flex-col-reverse md:flex-row justify-around items-center h-full">
+        <section className="h-[800px] bg-home bg-no-repeat bg-cover bg-center py-24">
+            <div className="container mx-auto flex flex-col-reverse md:flex-row justify-between items-center h-full px-4 md:px-64">
                 <div className='flex flex-col justify-center items-center text-center md:text-left'>
-                    <div className='font-semibold flex items-center uppercase'>
-                        <div className='w-10 h-[2px] bg-red-500 mr-3'></div>             
-                        new trend
+                    <div className='font-semibold flex items-center bg-modGreen text-white border-0 rounded-xl px-3 py-1 text-left'>
+                        <h1 className='text-[32px] md:text-[50px] leading-[1.1] font-light px-2 py-4'>
+                            <span className='font-normal drop-shadow-lg'>
+                                Start Exploring  <br />
+                            </span>
+                            <p className='mt-3 text-[20px] md:text-[24px]'>
+                                Our Amazing Product
+                            </p>
+                        </h1>
                     </div>
-                    <h1 className='text-[70px] leading-[1.1] font-light mb-4'>
-                        autumn <br />
-                        <span className='font-semibold'>women</span>
-                    </h1>
-                    <Link to={'/'} className='self-start uppercase font-semibold border-b-2 border-primary'>
-                        Discover More
-                    </Link>
+                    <button onClick={() => productsRef.current.scrollIntoView({ behavior: 'smooth' })}className="mt-4 self-start uppercase text-lg md:text-2xl font-extrabold text-white bg-primary px-8 py-3 rounded-full border-0 hover:bg-modOrange">
+                        Find More
+                    </button>
                 </div>
                 <div className='flex justify-center items-center mt-4 md:mt-0 md:ml-8'>
-                    <img className='w-[200px] sm:w-[300px] transition-all duration-300' src={Phone} alt="" />
+                    <img className='w-[150px] sm:w-[200px] md:w-[300px] transition-all duration-300' src={Phone} alt="" />
                 </div>
             </div>
         </section>
     );
 };
 
-export default Character;
+export default Landing;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import {useContext} from 'react';
-import {ProductContext} from '../contexts/ProductContext'; 
+import { useContext, useRef } from 'react';
+import { ProductContext } from '../contexts/ProductContext'; 
 import Product from '../components/Product';
 import Landing from '../components/Landing';
 
@@ -10,10 +10,12 @@ const Home = () => {
         return item.category;
     });
 
+    const productsRef = useRef(null);
+
     return (
     <div>
-        <Landing />
-        <section className='py-16'>
+        <Landing productsRef={productsRef} />
+        <section ref={productsRef} className='py-16'>
             <div className="container mx-auto">
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
                     {filteredProducts.map((product) => {
